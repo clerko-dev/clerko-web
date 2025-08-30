d84e6211b8df7c089f407aa23d5a3a7c3f01d637
clerko-dev <clerko.ops@gmail.com>
2025-08-30 02:28:21 +0200

    fix(home): close fragment and restore home sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,25 @@
-import HeroSection from "../components/home/HeroSection.jsx";
-import GeneratorSection from "../components/home/GeneratorSection.jsx";
+import SEO from "@/components/SEO.jsx";
+import HeroSection from "@/components/home/HeroSection.jsx";
+import HighlightsStrip from "@/components/home/HighlightsStrip.jsx"; // jeśli nie masz tego pliku, patrz punkt 1b
+import SocialProofSection from "@/components/home/SocialProofSection.jsx";
+import FeaturesSection from "@/components/home/FeaturesSection.jsx";
+import StepsSection from "@/components/home/StepsSection.jsx";
+import GeneratorSection from "@/components/home/GeneratorSection.jsx";
+import PricingSection from "@/components/home/PricingSection.jsx";
+import FAQSection from "@/components/home/FAQSection.jsx";
 
 export default function Home() {
   return (
     <>
+      <SEO title="Home" />
       <HeroSection />
-      import HighlightsStrip from "@/components/home/HighlightsStrip.jsx";
-// ...
-<>
-  <HeroSection />
-  import HighlightsStrip from "@/components/home/HighlightsStrip.jsx";
-// ...
-<>
-  <HeroSection />
-  <HighlightsStrip />
-  {/* dalej: Features/Steps/Generator/Pricing/FAQ... */}
-</>
-
+      <HighlightsStrip />
+      <SocialProofSection />
+      <FeaturesSection />
+      <StepsSection />
       <GeneratorSection />
+      <PricingSection />
+      <FAQSection />
     </>
   );
 }
