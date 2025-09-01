d70b3a9f43fa867378f6e7c1b65f4f0c71f5e7bf
clerko-dev <clerko.ops@gmail.com>
2025-09-01 20:29:01 +0200

    fix(how-to): remove duplicate default export and wrap Guides

--- src/pages/HowTo.jsx
+++ src/pages/HowTo.jsx
@@ -2,7 +2,5 @@ import React from "react";
 import Guides from "./Guides.jsx";
 
 export default function HowTo() {
-  // Jeden komponent źródłowy – zero duplikacji i zero błędu guidesFiltered
   return <Guides />;
 }
-export default guides;
