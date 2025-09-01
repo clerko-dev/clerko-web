6361786f24280dd69869ff899b080ed0bbbf535c
clerko-dev <clerko.ops@gmail.com>
2025-09-02 01:36:34 +0200

    ui(home): remove metrics strip; show 'Why teams switch to Clerko' directly (2025-09-02 01:36:34)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import HeroSection from '@/components/home/HeroSection.jsx';
 import HowItWorks from '@/components/home/HowItWorks.jsx';
-import HighlightsStrip from '@/components/home/HighlightsStrip.jsx';
 import BenefitsSection from '@/components/home/BenefitsSection.jsx';
 import TrustedSection from '@/components/home/TrustedSection.jsx';
 import CTASection from '@/components/home/CTASection.jsx';
@@ -10,7 +9,6 @@ export default function Home() {
     <>
       <HeroSection />
       <HowItWorks />
-      <HighlightsStrip />   {/* 58s / 40+ / 92% na gradientowym pasie */}
       <BenefitsSection />
       <TrustedSection />
       <CTASection />
