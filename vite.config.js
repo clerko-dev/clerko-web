3a26331840b6dc29e7ef453482344f910d2b72b7
clerko-dev <clerko.ops@gmail.com>
2025-09-01 21:05:23 +0200

    build: fix alias @ -> src (2025-09-01 21:05:23)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
   build: { sourcemap: true },
   define: {
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
