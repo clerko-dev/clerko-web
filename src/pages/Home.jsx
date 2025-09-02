f93c1fcb8de1f55945a71c5fe3bcf6b2a1f99b59
clerko-dev <clerko.ops@gmail.com>
2025-09-02 02:08:40 +0200

    layout(home): place BenefitsSection right below HowItWorks

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,13 +3,18 @@ import HowItWorks from '@/components/home/HowItWorks.jsx';
 import BenefitsSection from '@/components/home/BenefitsSection.jsx';
 import TrustedSection from '@/components/home/TrustedSection.jsx';
 import CTASection from '@/components/home/CTASection.jsx';
-
+import HighlightsStrip from '@/components/home/HighlightsStrip.jsx';
 export default function Home() {
   return (
     <>
       <HeroSection />
       <HowItWorks />
-      <BenefitsSection />
+<div className="mt-10 sm:mt-12">
+  <BenefitsSection />
+</div>
+<div className="mt-10 sm:mt-12">
+  <HighlightsStrip />
+</div>
       <TrustedSection />
       <CTASection />
     </>
